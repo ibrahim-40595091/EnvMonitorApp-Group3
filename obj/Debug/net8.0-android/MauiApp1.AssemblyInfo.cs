590fbefac48ddfda248f5760a4204e705eca4c17
Jinson <jinsonthayil@hotmail.co.uk>
2025-05-04 18:11:57 +0100

    just checking fucntionality

--- obj/Debug/net8.0-android/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ab91c469a9e90bfdf6a809c6911c2eb73b8648c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+88c603bd3287eca29cc5d2d6386099f3d8d5eb7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
