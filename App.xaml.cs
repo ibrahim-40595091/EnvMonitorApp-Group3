ab70fe2071d382ab456a33bbb670cb105efb06d2
Abdirahman <40628520@live.napier.ac.uk>
2025-05-04 21:41:00 +0100

    login page made

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiApp1
+﻿using MauiApp1.Views; 
+
+namespace MauiApp1
 {
     public partial class App : Application
     {
@@ -6,7 +8,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            // Show login page first
+            MainPage = new LoginPage();
         }
     }
 }
