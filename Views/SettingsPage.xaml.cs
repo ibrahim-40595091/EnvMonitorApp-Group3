6b67517f81661eba3dbcd425525d6f58a52e1233
Ibrahim <ibrahim.iby12@gmail.com>
2025-05-04 22:14:54 +0100

    created button save data in settings

---
+++ Views/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiApp1.Views;
+
+public partial class SettingsPage : ContentPage
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnSaveClicked(object sender, EventArgs e)
+    {
+        var saveTime = DateTime.Now.ToString("HH:mm:ss tt, MMM dd yyyy");
+        LastSavedLabel.Text = $"Last saved: {saveTime}";
+    }
+}
