66c74a940eb16c61104659f6be507a8f50cfb402
Jinson <jinsonthayil@hotmail.co.uk>
2025-05-04 21:42:27 +0100

    small changes

--- obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+eda53099bf0b5e2999f71a14943a087bb7b253d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a4afbfad455ef931324e91a9df8726c52ef33be3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
