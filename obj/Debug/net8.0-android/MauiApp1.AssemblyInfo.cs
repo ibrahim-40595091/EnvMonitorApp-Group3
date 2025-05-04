d42c037ac60bbcbba7c3d003e6da0f18f066f119
Jinson <jinsonthayil@hotmail.co.uk>
2025-05-04 20:14:06 +0100

    xml change

--- obj/Debug/net8.0-android/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2aa1312536967fefcb590a7780e8f1a88476ab1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8275510542cbc5d438bef0bf351ccc18ec6c6b19")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
